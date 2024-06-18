cad3610da72d6e41a82c424d8edf3bf2b9a91698
janpelto <jan.peltoniemi@hotmail.com>
2024-06-18 19:16:34 +0300

    Made product categories collapsible

--- javascript/tuotteet.js
+++ javascript/tuotteet.js
@@ -9,25 +9,53 @@ const fetchProducts = async () => {
                 const categoryDiv = document.createElement("div");
                 categoryDiv.className = "product-category";
 
+                const categoryTitleContainer = document.createElement("div");
+                categoryTitleContainer.className = "category-title-container";
                 const title = document.createElement("h4");
+                title.className = "category-title";
                 title.textContent = kategoria.categoryName;
-                categoryDiv.appendChild(title);
+
+                const arrowIcon = document.createElement("img");
+                arrowIcon.className = "expand-collapse-category"
+                arrowIcon.src = "../media/icons/plus-circle.svg";
+                arrowIcon.alt = "expand";
+                arrowIcon.style.height = "25px";
+                arrowIcon.style.width = "25px";
+                arrowIcon.addEventListener("click", function () {
+                    var x = this.parentElement;
+                    var sibling = x.nextElementSibling;
+                    if(sibling.className == "brand-cards-container-hidden"){
+                        sibling.className = "brand-cards-container";
+                        var icon = this;
+                        icon.src = "../media/icons/dash-circle.svg";
+                    }
+                    else{
+                        sibling.className = "brand-cards-container-hidden";
+                        var iconn = this;
+                        iconn.src = "../media/icons/plus-circle.svg";
+                    }
+                });
+
+                categoryTitleContainer.appendChild(title);
+                categoryTitleContainer.appendChild(arrowIcon);
+
+                categoryDiv.appendChild(categoryTitleContainer);
 
                 const brandCardsContainer = document.createElement("div");
-                brandCardsContainer.className = "brand-cards-container"
+                brandCardsContainer.className = "brand-cards-container-hidden"
                 categoryDiv.appendChild(brandCardsContainer)
 
                 productsContainer[0].appendChild(categoryDiv);
 
                 data.brandList.forEach(merkki => {
-                    let brandContainer = document.getElementsByClassName("brand-cards-container");
+                    let brandContainer = document.getElementsByClassName("brand-cards-container-hidden");
                     if (kategoria.categoryId == merkki.categoryId) {
                         var card = document.createElement("div");
                         card.classList.add("product-card");
 
                         var productInfo = document.createElement("div");
                         productInfo.classList.add("product-info");
-                        var brandName = document.createElement("h2");
+                        var brandName = document.createElement("h3");
                         brandName.innerText = merkki.brandName;
 
                         var productWebsite = document.createElement("div");
